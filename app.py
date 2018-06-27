e729bdbe40efab138a8200eeca84bcd9145f9e64
OnoAyumu <airagu950@gmail.com>
2018-06-27 17:02:35 +0900

    improve

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-        TextSendMessage("aaa")
+        TextSendMessage(text = 'aaa')
         print("message")
         
 if __name__ == "__main__":
