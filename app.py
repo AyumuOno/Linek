e147246affef4546924b31b7f93e52e26a49f40b
OnoAyumu <airagu950@gmail.com>
2018-06-27 16:22:05 +0900

    improve

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import os
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
 handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
 
-@app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=['GET'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
