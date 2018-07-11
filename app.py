bfe0017b3996cdfc3aab83054ff37674c61317c0
OnoAyumu <airagu950@gmail.com>
2018-07-11 16:10:43 +0900

    1st commit

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ def callback():
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    print("success")
 
     # handle webhook body
     try:
